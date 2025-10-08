3db512324de8dd56cbb499b20d5400de47433ad1
unknown <C00299313@setu.ie>
2025-10-08 10:14:03 +0100

    Updated code and added css

--- connect4_textual.py
+++ connect4_textual.py
@@ -4,7 +4,7 @@ from textual.widgets import Button, Static, Header, Footer
 
 COLS, ROWS = 6, 7
 
-class Cell (Static)
+class Cell (Static):
     def set_token(self, t: str):
         self.update(t if t != " " else " ")
         self.remove_class("x")
@@ -23,4 +23,70 @@ class Connect4App(App):
         ("5", "drop(4)", "Drop 5"),
         ("6", "drop(5)", "Drop 6"),
         ("7", "drop(6)", "Drop 7"),
-    ]
+        ]
+    
+    def __init__(self):
+        super().__init__()
+        # Matrix of cell
+        self.board = [[" "] * COLS for _ in range(ROWS)]
+        self.turn = "X"
+        self.cells: list[list[Cell]] = [[None]*COLS for _ in range(ROWS)]
+    
+    # Build HUD of the game
+    def compose(self) -> ComposeResult:
+        yield Header(show_clock=True)
+        with Vertical(id="root"):
+            with Horizontal(id="controls"):
+                for c in range(COLS):
+                    yield Button(str(c+1), id=f"col-{c}", variant = "primary")
+            with Grid(id="board"):
+                for r in range(ROWS-1, -1, -1):
+                    for c in range(COLS):
+                        cell = Cell(" ", classes="cell empty", id=f"cell-{r}-{c}")
+                        self.cells[r][c] = cell
+                        yield cell
+        yield Footer()
+    
+    # Game logic
+    def drop_token(self, col:int) -> bool:
+        if not (0 <= col < COLS):
+            return False
+        # find the first empty cell
+        for r in range(ROWS):
+            if self.board[r][col] == " ":
+                self.board[r][col] = self.turn
+                self.cells[r][col].set_token(self.turn)
+                return True
+            return False
+    
+    def switch_turn(self):
+        self.turn = "O" if self.turn == "X" else "X"
+        self.sub_title = f"Turn: {self.turn}"
+    
+    def action_drop(self, col: int):
+        if self.drop_token(col):
+            if self.check_win(self.turn):
+                self.sub_title = f"{self.turn} wins! (R to reset)"
+                for b in self.query(Button):
+                    b.disabled = True
+            else:
+                self.switch_turn()
+    
+    def action_reset(self):
+        self.board = [[" "] * COLS for _ in range(ROWS)]
+        for r in range(ROWS):
+            for c in range(COLS):
+                self.cells[r][c].set_token(" ")
+            self.turn = "X"
+            self.sub_title = f"Turn: {self.turn}"
+            for b in self.query(Button):
+                b.disabled = False
+    
+    async def on_button_pressed(self, event: Button.Pressed):
+        if event.button.id and event.button.id.startswith("col-"):
+            col = int(event.button.id.split("-")[1])
+            self.action_drop(col)
+    
+    
+if __name__ == "__main__":
+        Connect4App().run()
